406794a5d77fd743500effa8a642ec1113cf76f0
ysrtd <1059825313@qq.com>
2022-03-10 16:33:01 +0100

    Traverse all cells of perception to find the closest packet and move towards it

--- src/main/java/agent/behavior/myfirstagent/MyFirstAgent.java
+++ src/main/java/agent/behavior/myfirstagent/MyFirstAgent.java
@@ -9,6 +9,7 @@ import agent.AgentCommunication;
 import agent.AgentState;
 import agent.behavior.Behavior;
 import environment.Coordinate;
+import org.checkerframework.checker.units.qual.C;
 
 public class MyFirstAgent extends Behavior {
 
@@ -20,36 +21,69 @@ public class MyFirstAgent extends Behavior {
 
     @Override
     public void act(AgentState agentState, AgentAction agentAction) {
-        // Potential moves an agent can make (radius of 1 around the agent)
-        List<Coordinate> moves = new ArrayList<>(List.of(
-                new Coordinate(1, 1), new Coordinate(-1, -1),
-                new Coordinate(1, 0), new Coordinate(-1, 0),
-                new Coordinate(0, 1), new Coordinate(0, -1),
-                new Coordinate(1, -1), new Coordinate(-1, 1)
-        ));
+        // get perception
+        var perception = agentState.getPerception();
+        int width = perception.getWidth();
+        int height = perception.getHeight();
+        int offsetX = perception.getOffsetX();
+        int offsetY = perception.getOffsetY();
 
-        // Shuffle moves randomly
-        Collections.shuffle(moves);
+        // find the closest packet using manhattan distance and move towards it
+        if (!agentState.hasCarry() && agentState.seesPacket()) {
+            int man_dist = Integer.MAX_VALUE;
+            Coordinate closestPacket;
+            Coordinate move = new Coordinate(0,0);
+            for (int i = offsetX; i < offsetX + width; i++) {
+                for (int j = offsetY; j < offsetY + height; j++) {
+                    if (perception.getCellPerceptionOnAbsPos(i, j) != null
+                            && perception.getCellPerceptionOnAbsPos(i, j).containsPacket()) {
+                        int d = Math.abs(i - perception.getSelfX()) + Math.abs(j - perception.getSelfY());
+                        if (d < man_dist) {
+                            man_dist = d;
+                            closestPacket = new Coordinate(i, j);
+                            move = new Coordinate(
+                                    (int) Math.signum(i - perception.getSelfX()),
+                                    (int) Math.signum(j - perception.getSelfY()));
+                        }
+                    }
+                }
+            }
+            // take the move
+            if (perception.getCellPerceptionOnRelPos(move.getX(), move.getY()) != null
+                    && perception.getCellPerceptionOnRelPos(move.getX(), move.getY()).isWalkable()) {
+                agentAction.step(agentState.getX() + move.getX(), agentState.getY() + move.getY());
+                return;
+            }
+        } else {
+            // take a random move
+            // Potential moves an agent can make (radius of 1 around the agent)
+            List<Coordinate> moves = new ArrayList<>(List.of(
+                    new Coordinate(1, 1), new Coordinate(-1, -1),
+                    new Coordinate(1, 0), new Coordinate(-1, 0),
+                    new Coordinate(0, 1), new Coordinate(0, -1),
+                    new Coordinate(1, -1), new Coordinate(-1, 1)
+            ));
 
-        // Check for viable moves
-        for (var move : moves) {
-            var perception = agentState.getPerception();
-            int x = move.getX();
-            int y = move.getY();
+            // Shuffle moves randomly
+            Collections.shuffle(moves);
 
-            // If the area is null, it is outside the bounds of the environment
-            //  (when the agent is at any edge for example some moves are not possible)
-            if (perception.getCellPerceptionOnRelPos(x, y) != null) {
-                if (perception.getCellPerceptionOnRelPos(x, y).isWalkable()) {
+            // Check for viable moves
+            for (var move : moves) {
+                int x = move.getX();
+                int y = move.getY();
+
+                // If the area is null, it is outside the bounds of the environment
+                //  (when the agent is at any edge for example some moves are not possible)
+                if (perception.getCellPerceptionOnRelPos(x, y) != null && perception.getCellPerceptionOnRelPos(x, y).isWalkable()) {
                     agentAction.step(agentState.getX() + x, agentState.getY() + y);
-                    return;
-                } else if (!agentState.hasCarry() && perception.getCellPerceptionOnRelPos(x, y).containsPacket()) {
-                    agentAction.pickPacket(agentState.getX() + x, agentState.getY() + y);
                     return;
                 }
             }
+
         }
 
+
+        //agentAction.step(agentState.getX() + 1, agentState.getY() + 1);
         // No viable moves, skip turn
         agentAction.skip();
     }

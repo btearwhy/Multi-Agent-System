a10f501931a73554dc5abe547bf4a7594bfcd5a6
ysrtd <1059825313@qq.com>
2022-05-01 09:59:11 +0200

    DStarLite without Main()

--- src/main/java/agent/dstarlite/DStarLite.java
+++ src/main/java/agent/dstarlite/DStarLite.java
@@ -1,10 +1,10 @@
 package agent.dstarlite;
 
 import environment.Coordinate;
+import org.checkerframework.checker.units.qual.C;
 import util.Pair;
 
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class DStarLite implements java.io.Serializable{
     private PriorityQueueU queueU;
@@ -14,12 +14,24 @@ public class DStarLite implements java.io.Serializable{
     private Coordinate s_start;
     private Coordinate s_last;
     private Coordinate s_goal;
+    private HashSet<Coordinate> obstacles;
+    private final Coordinate[] eight_neighbors;
 
     public DStarLite() {
         queueU = new PriorityQueueU();
         k_m = 0;
         rhs = new HashMap<Coordinate, Double>();
         g = new HashMap<Coordinate, Double>();
+        obstacles = new HashSet<>();
+        eight_neighbors = new Coordinate[]{
+                new Coordinate(1,0),
+                new Coordinate(1,1),
+                new Coordinate(0,1),
+                new Coordinate(-1,1),
+                new Coordinate(-1,0),
+                new Coordinate(-1,-1),
+                new Coordinate(0,-1),
+                new Coordinate(1,-1)};
     }
 
     /**
@@ -33,22 +45,67 @@ public class DStarLite implements java.io.Serializable{
         return new PriorityKey(k1, k2);
     }
 
-    private int compareKey(Pair<Double, Double> k1, Pair<Double, Double> k2) {
-
+    private double getRHS(Coordinate u) {
+        if (!rhs.containsKey(u)) {
+            rhs.put(u, Double.POSITIVE_INFINITY);
+        }
+        return rhs.get(u);
     }
 
-    private double getRHS(Coordinate s) {
-        if (!rhs.containsKey(s)) {
-            rhs.put(s, Double.POSITIVE_INFINITY);
+    private double getG(Coordinate u) {
+        if (!g.containsKey(u)) {
+            g.put(u, Double.POSITIVE_INFINITY);
         }
-        return rhs.get(s);
+        return g.get(u);
     }
 
-    private double getG(Coordinate s) {
-        if (!g.containsKey(s)) {
-            g.put(s, Double.POSITIVE_INFINITY);
+    private ArrayList<Coordinate> getPred(Coordinate u) {
+        ArrayList<Coordinate> pred = new ArrayList<>();
+        if (obstacles.contains(u)) {
+            return pred;
         }
-        return g.get(s);
+        else {
+            for (Coordinate n:eight_neighbors) {
+                if (!obstacles.contains(u.add(n))) {
+                    pred.add(u.add(n));
+                }
+            }
+        }
+
+        return pred;
+    }
+
+    private ArrayList<Coordinate> getSucc(Coordinate u) {
+        ArrayList<Coordinate> succ = new ArrayList<>();
+        if (obstacles.contains(u)) {
+            return succ;
+        }
+        else {
+            for (Coordinate n:eight_neighbors) {
+                if (!obstacles.contains(u.add(n))) {
+                    succ.add(u.add(n));
+                }
+            }
+        }
+
+        return succ;
+    }
+
+    private double cost(Coordinate s1, Coordinate s2) {
+        int dx = Math.abs(s1.diff(s2).getX());
+        int dy = Math.abs(s1.diff(s2).getY());
+
+        assert (dx <= 1 && dy <= 1);
+
+        if (obstacles.contains(s1) || obstacles.contains(s2)) {
+            return Double.POSITIVE_INFINITY;
+        }
+        else if (dx == 0 && dy == 0) {
+            return 0.;
+        }
+        else {
+            return 1.;
+        }
     }
 
     private double heuristic(Coordinate s1, Coordinate s2) {
@@ -65,30 +122,74 @@ public class DStarLite implements java.io.Serializable{
         queueU.insert(s_goal, calculateKey(s_goal));
     }
 
+    public void updateVertex(Coordinate u) {
+        boolean containU = queueU.contains(u);
+        int compare = Double.compare(getG(u), getRHS(u));
+        if (compare != 0 && containU) {
+            queueU.update(u, calculateKey(u));
+        }
+        else if (compare != 0 && !containU) {
+            queueU.insert(u, calculateKey(u));
+        }
+        else if (compare == 0 && containU) {
+            queueU.remove(u);
+        }
+    }
+
     public void computeShortestPath() {
         while (!queueU.isEmpty() &&
                 (queueU.topKey().compareTo(calculateKey(s_start)) < 0 || getRHS(s_start) > getG(s_start))) {
             Coordinate u = queueU.top(); // {11"} u=U.Top()
             PriorityKey k_old = queueU.topKey();// {12"} k_old = U.TopKey()
-            VertexWithPriority s_new = calculateKey(s_old); // {13"}
-            if (s_old.compareTo(s_new) < 0) { // {14"}
-                // {15"} U.Update()
-                while (queueU.contains(s_old)) {
-                    queueU.remove(s_old);
-                }
-                queueU.add(s_new);
+            PriorityKey k_new = calculateKey(u); // {13"}
+            if (k_old.compareTo(k_new) < 0) { // {14"}
+                queueU.update(u, k_new); // {15"} U.Update()
             }
-            else if (getG(s_old) > getRHS(s_old)) { // {16"}
-                g.put(s_old, )
+            else if (getG(u) > getRHS(u)) { // {16"}
+                g.put(u, getRHS(u));
+                queueU.remove(u);
+                for (Coordinate s:getPred(u)) {
+                    if (!s.equals(s_goal)) {
+                        rhs.put(s, Math.min(getRHS(s), cost(s,u) + getG(u)));
+                    }
+                    updateVertex(s);
+                }
+            }
+            else {
+                double g_old = getG(u);
+                g.put(u, Double.POSITIVE_INFINITY);
+                ArrayList<Coordinate> list = getSucc(u);
+                list.add(u);
+                for (Coordinate s:list) {
+                    if (Double.compare(getRHS(s), (cost(s, u) + g_old)) == 0) {
+                        if (!s.equals(s_goal)) {
+                            // get min_{s_prime} cost(s, s_prime) + getG(s_prime)
+                            double min_val = Double.POSITIVE_INFINITY;
+                            for (Coordinate s_prime:getSucc(s)) {
+                                double val = cost(s, s_prime) + getG(s_prime);
+                                if (val < min_val) {
+                                    min_val = val;
+                                }
+                            }
+                            rhs.put(s, min_val);
+                        }
+                    }
+                    updateVertex(s);
+                }
             }
             System.out.println();
         }
+    }
+
+    public void run() {
+
     }
 
     public static void main(String[] args) {
         DStarLite dStarLite = new DStarLite();
         dStarLite.init(0,0,1,1);
         dStarLite.computeShortestPath();
+        System.out.println(Double.compare(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY));
     }
 
 }
